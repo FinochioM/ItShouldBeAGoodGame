2946020769bcac4c6cd7cf1cd807fc795f2cbde0
Matias <matias.finochio@davinci.edu.ar>
2025-06-05 12:33:29 -0300

    manually added ui, added fonts and made sprites smaller

--- itsgonnabeagoodgame/Scripts/main_scene.gd
+++ itsgonnabeagoodgame/Scripts/main_scene.gd
@@ -18,55 +18,54 @@ var timing_upgrade_base_cost: float = 10.0
 var amount_upgrade_base_cost: float = 50.0
 var upgrade_cost_multiplier: float = 1.5
 
-@onready var money_label: Label
-@onready var patty_count_label: Label
-@onready var timing_upgrade_button: Button
-@onready var amount_upgrade_button: Button
-@onready var timing_info_label: Label
-@onready var amount_info_label: Label
-@onready var grill_level_label: Label
+@onready var money_label: Label = $UI/MoneyLabel
+@onready var patty_count_label: Label = $UI/PattyCountLabel
+@onready var grill_level_label: Label = $UI/GrillLevelLabel
+@onready var timing_upgrade_button: TextureButton = $UI/TimingUpgradeSection/UpgradeButton
+@onready var amount_upgrade_button: TextureButton = $UI/AmountUpgradeSection/UpgradeButton
+@onready var timing_info_label: Label = $UI/TimingUpgradeSection/Information
+@onready var amount_info_label: Label = $UI/AmountUpgradeSection/Information
 
 var counted_patties = []
 
 func _ready():
 	spawn_position = Vector2(240, 100)
 	update_spawn_interval()
-	create_ui()
-	
+
 	var money_timer = Timer.new()
 	money_timer.wait_time = 1.0
 	money_timer.timeout.connect(_on_money_timer_timeout)
 	money_timer.autostart = true
 	add_child(money_timer)
-	
+
 func _process(delta):
 	spawn_timer += delta
 	if spawn_timer >= current_spawn_interval:
 		spawn_patty()
 		spawn_timer = 0.0
-		
+
 	update_ui()
 	update_upgrade_buttons()
 
 func spawn_patty():
 	var patty = patty_scene.instantiate()
-	
+
 	var random_offset = randf_range(-5, 5)
 	patty.position = Vector2(spawn_position.x + random_offset, spawn_position.y)
-	
+
 	patty.patty_landed.connect(_on_patty_landed.bind(patty))
-	
+
 	add_child(patty)
 	print("PATTY SPAWNED!")
-	
+
 func _on_patty_landed(patty):
 	if patty in counted_patties:
 		print("PATTY ALREADY COUNTED _ IGNORING")
 		return
-	
+
 	counted_patties.append(patty)
 	patty_count += patty_multiplier
-	print("PATTY LANDED IN THE GRILL: TOTAL PATTIES = ", patty_count)		
+	print("PATTY LANDED IN THE GRILL: TOTAL PATTIES = ", patty_count)
 
 func _on_money_timer_timeout():
 	var earnings = patty_count * money_per_second_per_patty
@@ -96,6 +95,7 @@ func get_timing_upgrades_count() -> int:
 	return count
 
 func get_amount_upgrades_count() -> int:
+	@warning_ignore("integer_division")
 	return grill_level / 5
 
 func update_spawn_interval():
@@ -122,78 +122,8 @@ func purchase_amount_upgrade():
 
 func update_upgrade_buttons():
 	timing_upgrade_button.disabled = not can_timing_upgrade() or money < get_timing_upgrade_cost()
-	
+
 	amount_upgrade_button.disabled = not can_amount_upgrade() or money < get_amount_upgrade_cost()
-
-func create_ui():
-	var canvas_layer = CanvasLayer.new()
-	add_child(canvas_layer)
-	
-	money_label = Label.new()
-	money_label.position = Vector2(10, 10)
-	money_label.add_theme_font_size_override("font_size", 24)
-	canvas_layer.add_child(money_label)
-	
-	patty_count_label = Label.new()
-	patty_count_label.position = Vector2(10, 40)
-	patty_count_label.add_theme_font_size_override("font_size", 24)
-	canvas_layer.add_child(patty_count_label)
-	
-	grill_level_label = Label.new()
-	grill_level_label.position = Vector2(190, 10)
-	grill_level_label.add_theme_font_size_override("font_size", 24)
-	canvas_layer.add_child(grill_level_label)
-	
-	create_upgrade_ui(canvas_layer)
-
-func create_upgrade_ui(canvas_layer: CanvasLayer):
-	var timing_section = Panel.new()
-	timing_section.position = Vector2(320, 20)
-	timing_section.size = Vector2(150, 100)
-	timing_section.modulate = Color(0.8, 0.8, 0.8, 0.7)
-	canvas_layer.add_child(timing_section)
-	
-	var timing_title = Label.new()
-	timing_title.position = Vector2(10, 5)
-	timing_title.text = "SPEED UPGRADE"
-	timing_title.add_theme_font_size_override("font_size", 12)
-	timing_section.add_child(timing_title)
-	
-	timing_info_label = Label.new()
-	timing_info_label.position = Vector2(10, 25)
-	timing_info_label.add_theme_font_size_override("font_size", 10)
-	timing_section.add_child(timing_info_label)
-	
-	timing_upgrade_button = Button.new()
-	timing_upgrade_button.position = Vector2(10, 70)
-	timing_upgrade_button.size = Vector2(130, 25)
-	timing_upgrade_button.text = "UPGRADE"
-	timing_upgrade_button.pressed.connect(purchase_timing_upgrade)
-	timing_section.add_child(timing_upgrade_button)
-	
-	var amount_section = Panel.new()
-	amount_section.position = Vector2(320, 140)
-	amount_section.size = Vector2(150, 100)
-	amount_section.modulate = Color(0.8, 0.8, 0.8, 0.7)
-	canvas_layer.add_child(amount_section)
-	
-	var amount_title = Label.new()
-	amount_title.position = Vector2(10, 5)
-	amount_title.text = "AMOUNT UPGRADE"
-	amount_title.add_theme_font_size_override("font_size", 12)
-	amount_section.add_child(amount_title)
-	
-	amount_info_label = Label.new()
-	amount_info_label.position = Vector2(10, 25)
-	amount_info_label.add_theme_font_size_override("font_size", 10)
-	amount_section.add_child(amount_info_label)
-	
-	amount_upgrade_button = Button.new()
-	amount_upgrade_button.position = Vector2(10, 70)
-	amount_upgrade_button.size = Vector2(130, 25)
-	amount_upgrade_button.text = "UPGRADE"
-	amount_upgrade_button.pressed.connect(purchase_amount_upgrade)
-	amount_section.add_child(amount_upgrade_button)
 
 func update_ui():
 	if money_label:
@@ -202,7 +132,7 @@ func update_ui():
 		patty_count_label.text = "PATTIES: %d" % patty_count
 	if grill_level_label:
 		grill_level_label.text = "GRILL LEVEL: %d" % grill_level
-	
+
 	if timing_info_label:
 		if can_timing_upgrade():
 			var cost = get_timing_upgrade_cost()
@@ -211,7 +141,7 @@ func update_ui():
 		else:
 			var levels_until_next = 5 - (grill_level % 5)
 			timing_info_label.text = "Available in\n%d levels\n(Level %d)" % [levels_until_next, grill_level + levels_until_next]
-	
+
 	if amount_info_label:
 		if can_amount_upgrade():
 			var cost = get_amount_upgrade_cost()
--- itsgonnabeagoodgame/Scripts/patty.gd
+++ itsgonnabeagoodgame/Scripts/patty.gd
@@ -10,7 +10,7 @@ var landing_check_duration = 0.1
 func _ready() -> void:
 	contact_monitor = true
 	max_contacts_reported = 10
-	
+
 	collision_layer = 1
 	collision_mask = 1
 
@@ -18,22 +18,22 @@ func _ready() -> void:
 func _physics_process(delta: float):
 	if not has_landed:
 		var current_velocity = linear_velocity.length()
-		
+
 		if current_velocity < velocity_threshold:
 			landing_check_timer += delta
 			if landing_check_timer >= landing_check_duration:
 				_land()
 		else:
-			landing_check_timer = 0.0					
+			landing_check_timer = 0.0
 
 func _land():
 	if not has_landed:
 		has_landed = true
-		
+
 		freeze = true
 		freeze_mode = RigidBody2D.FREEZE_MODE_STATIC
-		
+
 		patty_landed.emit()
 		print("PATTY HAS LANDED")
-		
+
 		modulate = Color(0.9, 0.9, 0.9, 1.0)
