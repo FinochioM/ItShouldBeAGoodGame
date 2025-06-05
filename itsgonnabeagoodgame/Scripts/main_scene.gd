b29b8a7643a0cbb0c62dc0a4ff2f0d651adfbbed
Matias <matias.finochio@davinci.edu.ar>
2025-06-05 08:09:55 -0300

    added textures and basic logic

---
+++ itsgonnabeagoodgame/Scripts/main_scene.gd
@@ -0,0 +1,74 @@
+extends Node2D
+
+var patty_scene = preload("res://Scenes/patty.tscn")
+
+var money: float = 0.0
+var patty_count: int = 0
+var money_per_second_per_patty: float = 0.1
+
+var spawn_timer: float = 0.0
+var spawn_interval: float = 10.0
+var spawn_position: Vector2
+
+@onready var money_label: Label
+@onready var patty_count_label: Label
+
+func _ready():
+	spawn_position = Vector2(240, 100)
+	
+	create_ui()
+	
+	var money_timer = Timer.new()
+	money_timer.wait_time = 1.0
+	money_timer.timeout.connect(_on_money_timer_timeout)
+	money_timer.autostart = true
+	add_child(money_timer)
+	
+func _process(delta):
+	spawn_timer += delta
+	if spawn_timer >= spawn_interval:
+		spawn_patty()
+		spawn_timer = 0.0
+		
+	update_ui()
+
+func spawn_patty():
+	var patty = patty_scene.instantiate()
+	
+	var random_offset = randf_range(-20, 20)
+	patty.position = Vector2(spawn_position.x + random_offset, spawn_position.y)
+	
+	patty.body_entered.connect(_on_patty_landed)
+	
+	add_child(patty)
+	print("PATTY SPAWNED!")
+	
+func _on_patty_landed(body):
+	patty_count += 1
+	print("PATTY LANDED IN THE GRILL: TOTAL PATTIES = ", patty_count)			
+
+func _on_money_timer_timeout():
+	var earnings = patty_count * money_per_second_per_patty
+	money += earnings
+	if earnings > 0:
+		print("EARNED ", earnings, " TOTAL MONEY", money)
+		
+func create_ui():
+	var canvas_layer = CanvasLayer.new()
+	add_child(canvas_layer)
+	
+	money_label = Label.new()
+	money_label.position = Vector2(10, 10)
+	money_label.add_theme_font_size_override("font_size", 24)
+	canvas_layer.add_child(money_label)
+	
+	patty_count_label = Label.new()
+	patty_count_label.position = Vector2(10, 40)
+	patty_count_label.add_theme_font_size_override("font_size", 24)
+	canvas_layer.add_child(patty_count_label)
+
+func update_ui():
+	if money_label:
+		money_label.text ="MONEY: $%.2f" % money
+	if patty_count_label:
+		patty_count_label.text = "PATTIES %d" % patty_count
---
+++ itsgonnabeagoodgame/Scripts/patty.gd
@@ -0,0 +1,35 @@
+extends RigidBody2D
+
+signal patty_landed
+
+var has_landed = false
+var landing_check_timer = 0.0
+var velocity_threshold = 10.0
+var landing_check_duration = 1.0
+
+func _ready() -> void:
+	contact_monitor = true
+	max_contacts_reported = 10
+	
+	collision_layer = 1
+	collision_mask = 1
+
+
+func _physics_process(delta: float):
+	if not has_landed:
+		var current_velocity = linear_velocity.length()
+		
+		if current_velocity < velocity_threshold:
+			landing_check_timer += delta
+			if landing_check_timer >= landing_check_duration:
+				_land()
+			else:
+				landing_check_timer = 0.0					
+
+func _land():
+	if not has_landed:
+		has_landed = true
+		patty_landed.emit()
+		print("PATTY HAS LANDED")
+		
+		modulate = Color(0.9, 0.9, 0.9, 1.0)
